c66a3bd15325acb0b81fe5b9092a3df502fe7f63
jinook929 <jinook929@gmail.com>
2021-06-28 19:20:18 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>This is a simple React App for deploying to Netlify</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
